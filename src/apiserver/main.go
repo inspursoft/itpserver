d949d9067fe81b062c1ee285146161415f5316a7
wangkun_lc <wangkun_lc@inspur.com>
2019-06-05 18:25:28 +0800

    Update for router config.

--- src/apiserver/main.go
+++ src/apiserver/main.go
@@ -4,7 +4,6 @@ import (
 	"path/filepath"
 
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/plugins/cors"
 	"github.com/inspursoft/itpserver/src/apiserver/dao"
 	_ "github.com/inspursoft/itpserver/src/apiserver/routers"
 )
@@ -17,13 +16,6 @@ func main() {
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 	beego.LoadAppConfig("ini", filepath.Join(appPath, "app.conf"))
-	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "HEAD"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}))
 	dao.InitDB()
 	beego.Run()
 }
