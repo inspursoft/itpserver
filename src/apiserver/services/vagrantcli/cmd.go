43a34b6c1cc0ca17dd67c0d1210407b5b9f1e5ab
wangkun <wangkun_lc@inspur.com>
2020-03-10 12:41:00 +0800

    Update for retrieving vm IP

--- src/apiserver/services/vagrantcli/cmd.go
+++ src/apiserver/services/vagrantcli/cmd.go
@@ -283,7 +283,7 @@ func (vc *vagrantCli) Destroy() error {
 	cli.executeCommand(fmt.Sprintf("%s destroy -f %s", vagrantCommand, vc.vmWithSpec.Spec.VID), true)
 	cli.executeCommand(fmt.Sprintf("rm -rf %s ", vc.workPath), true)
 	cli.executeCommand(fmt.Sprintf("rm -f %s", path.Join(vc.outputPath, vc.vmWithSpec.Name+".box")), true)
-	cli.executeCommand(fmt.Sprintf("sed /%s/d $HOME/.ssh/known_hosts", vc.vmWithSpec.IP), true)
+	cli.executeCommand(fmt.Sprintf("sed /%s/d ~/.ssh/known_hosts", vc.vmWithSpec.IP), true)
 	cli.remove()
 	if !vc.err.HasNoError() && vc.err != nil {
 		beego.Error(fmt.Sprintf("Failed to destroy VM with error: %+v", vc.err))
