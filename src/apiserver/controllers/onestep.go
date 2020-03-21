5f33007a93d6667afd4b4a4c6b41eb52b6860688
wangkun_lc <wangkun_lc@inspur.com>
2020-03-21 14:16:09 +0800

    Update for auth when in onestep.

--- src/apiserver/controllers/onestep.go
+++ src/apiserver/controllers/onestep.go
@@ -25,9 +25,9 @@ type OneStepController struct {
 func (ic *OneStepController) Post() {
 	var oneStep models.OneStepInstallation
 	ic.loadRequestBody(&oneStep)
-	ic.proxiedRequest(http.MethodPost, oneStep.VMWithSpec, "VMController.CreateBySpec")
-	ic.proxiedRequest(http.MethodPost, oneStep.PackageVO, "InstallationController.Post", ":vm_name", oneStep.VMWithSpec.Name)
-	ic.proxiedRequest(http.MethodPost, nil, "VMController.Package", ":vm_name", oneStep.VMWithSpec.Name)
+	ic.proxiedRequest(http.MethodPost, oneStep.VMWithSpec, "VMController.CreateBySpec", "access_token", "BOARD")
+	ic.proxiedRequest(http.MethodPost, oneStep.PackageVO, "InstallationController.Post", ":vm_name", oneStep.VMWithSpec.Name, "access_token", "BOARD")
+	ic.proxiedRequest(http.MethodPost, nil, "VMController.Package", ":vm_name", oneStep.VMWithSpec.Name, "access_token", "BOARD")
 }
 
 // @Title Post
@@ -44,6 +44,6 @@ func (ic *OneStepController) Post() {
 // @router /:vm_name [post]
 func (ic *OneStepController) PostWithVagrantfile() {
 	vmName := ic.requiredParam(":vm_name")
-	ic.proxiedRequest(http.MethodPost, nil, "VMController.CreateByVagrantfile", ":vm_name", vmName)
-	ic.proxiedRequest(http.MethodPost, nil, "VMController.Package", ":vm_name", vmName)
+	ic.proxiedRequest(http.MethodPost, nil, "VMController.CreateByVagrantfile", ":vm_name", vmName, "access_token", "BOARD")
+	ic.proxiedRequest(http.MethodPost, nil, "VMController.Package", ":vm_name", vmName, "access_token", "BOARD")
 }
